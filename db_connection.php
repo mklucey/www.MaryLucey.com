dc6f20ed5c6e961ca59462919e93bb4c7e122401
Your Name <you@example.com>
2023-11-22 15:11:12 -0600

    Update MySQL database name to 'if0_35418598_mydatabase'

--- db_connection.php
+++ db_connection.php
@@ -4,6 +4,7 @@
 $host = 'sql301.infinityfree.com';
 $username = 'if0_35418598'; // Your MySQL username
 $password = '7GPhBMcGSS'; // Your MySQL password
+$database = 'if0_35418598_mydatabase'; // Your MySQL database name
 
 
 ?>
