d819ea7477925a2ab054e52b2f318b102cedd076
Your Name <you@example.com>
2023-11-24 11:25:37 -0600

    Introduce response handling for profile picture update in 'profile_picture.php'.

--- profile_picture.php
+++ profile_picture.php
@@ -16,6 +16,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	// Update the profile picture in the database
     $query = "UPDATE users SET profile_picture='$newProfilePicture' WHERE username='{$_SESSION['username']}'";
     $result = $mysqli->query($query);
+	
+	if ($result) {
+        echo 'Profile picture updated successfully';
+    } else {
+        echo 'Failed to update profile picture';
+    }
+}
 
 
 
