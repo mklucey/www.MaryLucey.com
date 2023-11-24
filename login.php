8265b0559e0162548c7e82b2df10f01da67d8e41
Your Name <you@example.com>
2023-11-23 21:43:48 -0600

    Implement SELECT query for user retrieval by username

--- login.php
+++ login.php
@@ -8,6 +8,9 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
 	$password = $_POST['password'];
+	
+	 $query = "SELECT * FROM users WHERE username='$username'";
+     $result = $mysqli->query($query);
 
 
 ?>
