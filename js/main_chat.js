08769eca0a7d629ab28633418d6cf01110ded0f2
Your Name <you@example.com>
2023-11-25 17:04:49 -0600

    Access message input element by ID for user input handling.

--- js/main_chat.js
+++ js/main_chat.js
@@ -1,3 +1,4 @@
 // main_chat.js
 document.addEventListener('DOMContentLoaded', function() {
 	const messageForm = document.getElementById('messageForm');
+	const messageInput = document.getElementById('messageInput');
