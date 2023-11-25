35a2df5b33ecffb2587b84fab2ea77478d0b0ef0
Your Name <you@example.com>
2023-11-25 10:22:34 -0600

    Create SQL query to insert user data into 'users' table, including username, password, and email address.

--- signup.php
+++ signup.php
@@ -6,10 +6,12 @@ include 'db_connection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$username = $_POST['username'];
 	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	$email_address = $_POST['email_address'];
+	
 	
 	 // Perform input validation if needed
 	 
-	 $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+	 $query = "INSERT INTO users (username, password, email_address) VALUES ('$username', '$password', '$email_address')";
      $result = $mysqli->query($query);
 	 
 	 if ($result) {
