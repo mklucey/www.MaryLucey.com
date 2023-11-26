615f4c37f364706f6d84dc210f4152c4fed22014
Your Name <you@example.com>
2023-11-26 16:03:26 -0600

    Define a function to send a login request to the server. The function uses an XMLHttpRequest to handle server communication and processes the server response accordingly. Configure the XMLHttpRequest to send a POST request to 'login.php' asynchronously. Set the request header to 'application/x-www-form-urlencoded' for proper handling of POST data. Send the encoded username and password as part of the POST request to the server.

--- js/login.js
+++ js/login.js
@@ -19,3 +19,24 @@ document.addEventListener('DOMContentLoaded', function() {
         usernameInput.value = '';
         passwordInput.value = '';
     });
+	
+	 // Function to send a login request to the server
+    function login(username, password) {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    // Redirect or update the UI based on the server response
+                    console.log('Login successful');
+                } else {
+                    // Handle error response from the server
+                    console.error('Failed to login');
+                }
+            }
+        };
+
+        xhr.open('POST', 'login.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send('username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password));
+    }
+});
