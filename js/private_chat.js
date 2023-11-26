e0488c38aa0af6a321529fe07c9ff734f1c434b2
Your Name <you@example.com>
2023-11-25 21:51:16 -0600

    Access the private message input and private messages container elements by ID for handling private messages.

--- js/private_chat.js
+++ js/private_chat.js
@@ -1,3 +1,6 @@
 // private_chat.js
 document.addEventListener('DOMContentLoaded', function() {
 	const privateMessageForm = document.getElementById('privateMessageForm');
+	const privateMessageInput = document.getElementById('privateMessageInput');
+    const privateMessagesContainer = document.getElementById('privateMessages');
+
