edb5583e2c2d38771f500f541a3e56971fea2e3f
Your Name <you@example.com>
2023-11-26 15:44:03 -0600

    Retrieve the username and password input elements by ID for event handling.

--- js/login.js
+++ js/login.js
@@ -1,2 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const loginForm = document.getElementById('loginForm');
+	const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
