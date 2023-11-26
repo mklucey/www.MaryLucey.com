3f6539dd92d25d014ac5e40312db47c328f1358b
Your Name <you@example.com>
2023-11-26 15:49:29 -0600

    Attach submit event listener to loginForm for handling login form submissions. Prevent the default form submission behavior.

--- js/login.js
+++ js/login.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	const loginForm = document.getElementById('loginForm');
 	const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
+	
+	 // Event listener for submitting the login form
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
