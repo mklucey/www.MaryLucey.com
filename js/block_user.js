3b55d0e0e402de3eb134f17f303440c0e3ede937
Your Name <you@example.com>
2023-11-25 22:46:54 -0600

    Implement event listener for DOMContentLoaded to initiate script execution. Retrieve the block user form element and the blocked username input by ID for event handling.

---
+++ js/block_user.js
@@ -0,0 +1,4 @@
+// block_user.js
+document.addEventListener('DOMContentLoaded', function() {
+    const blockUserForm = document.getElementById('blockUserForm');
+    const blockedUsernameInput = document.getElementById('blocked_username');
