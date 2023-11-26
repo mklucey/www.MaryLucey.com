2ac8e5075860dbd2598b22208e3cc78941762573
Your Name <you@example.com>
2023-11-26 16:26:41 -0600

    Attach submit event listener to addFriendForm for handling friend request submissions. Prevent the default form submission behavior. Capture and trim the value of the friendUsername input for further processing. If the friend username is not empty, send the friend request to the server using AJAX. Clear the input field after a successful submission.

--- js/add_friend.js
+++ js/add_friend.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const addFriendForm = document.getElementById('addFriendForm');
     const friendUsernameInput = document.getElementById('friend_username');
+	
+	 // Event listener for submitting a friend request
+    addFriendForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const friendUsername = friendUsernameInput.value.trim();
+
+        if (friendUsername !== '') {
+            // Send the friend request to the server using AJAX
+            sendFriendRequest(friendUsername);
+        }
+
+        // Clear the input field
+        friendUsernameInput.value = '';
+    });
