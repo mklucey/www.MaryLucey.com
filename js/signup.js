335de54f17ed3c6715a5f4dee6d7145ead743eee
Your Name <you@example.com>
2023-11-26 11:03:49 -0600

    Access the password input element by ID for user input handling.

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,4 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const signupForm = document.getElementById('signupForm');
 	const usernameInput = document.getElementById('username');
+	const passwordInput = document.getElementById('password');
