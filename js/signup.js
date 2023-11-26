23659e3848415dda65f4a600e071b1fd8b5ca1ef
Your Name <you@example.com>
2023-11-26 13:58:40 -0600

    Define a function to send a signup request to the server. The function uses an XMLHttpRequest to handle server communication and processes the server response accordingly.

--- js/signup.js
+++ js/signup.js
@@ -12,3 +12,30 @@ document.addEventListener('DOMContentLoaded', function() {
 		const username = usernameInput.value.trim();
         const password = passwordInput.value.trim();
         const emailAddress = emailAddressInput.value.trim();
+		
+		 if (username !== '' && password !== '' && emailAddress !== '') {
+            // Send the signup request to the server using AJAX
+            signup(username, password, emailAddress);
+         }
+
+        // Clear the input fields
+        usernameInput.value = '';
+        passwordInput.value = '';
+        emailAddressInput.value = '';
+    });
+	
+	// Function to send a signup request to the server
+    function signup(username, password, emailAddress) {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    // Redirect or update the UI based on the server response
+                    console.log('Signup successful');
+                } else {
+                    // Handle error response from the server
+                    console.error('Failed to signup');
+                }
+            }
+        };
+
