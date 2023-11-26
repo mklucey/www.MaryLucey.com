733f59815e8a99aa7ac306ba80e90d322821e4b2
Your Name <you@example.com>
2023-11-25 22:51:18 -0600

    Implement an event listener for DOMContentLoaded to initiate script execution. Retrieve the block user form element and the blocked username input by ID for event handling. Add an event listener to the blockUserForm for handling the submission of block user requests and prevent the default form submission behavior. Capture and trim the value of the blockedUsername input for further processing. If the blocked username is not empty, send the block user request to the server using AJAX. Clear the input field after a successful submission.

--- js/block_user.js
+++ js/block_user.js
@@ -2,3 +2,39 @@
 document.addEventListener('DOMContentLoaded', function() {
     const blockUserForm = document.getElementById('blockUserForm');
     const blockedUsernameInput = document.getElementById('blocked_username');
+
+    // Event listener for submitting a block user request
+    blockUserForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const blockedUsername = blockedUsernameInput.value.trim();
+
+        if (blockedUsername !== '') {
+            // Send the block user request to the server using AJAX
+            blockUser(blockedUsername);
+        }
+
+        // Clear the input field
+        blockedUsernameInput.value = '';
+    });
+
+    // Function to send a block user request to the server
+    function blockUser(blockedUsername) {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    // Update the UI or handle success as needed
+                    console.log('User blocked successfully');
+                } else {
+                    // Handle error response from the server
+                    console.error('Failed to block user');
+                }
+            }
+        };
+
+        xhr.open('POST', 'block_user.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send('blocked_username=' + encodeURIComponent(blockedUsername));
+    }
+});
