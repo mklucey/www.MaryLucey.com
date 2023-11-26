ac8a74a0c796db5eaabc3f9bda859abe831bb7f3
Your Name <you@example.com>
2023-11-26 15:55:37 -0600

    Capture and trim the values of the username and password inputs for further processing. If both the username and password are not empty, send the login request to the server using AJAX. Clear the input fields after a successful submission.

--- js/login.js
+++ js/login.js
@@ -6,3 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
 	 // Event listener for submitting the login form
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
+		
+		const username = usernameInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        if (username !== '' && password !== '') {
+            // Send the login request to the server using AJAX
+            login(username, password);
+        }
+
+        // Clear the input fields
+        usernameInput.value = '';
+        passwordInput.value = '';
+    });
