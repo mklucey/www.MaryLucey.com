a7e2d41d988eb929a701cd09ab512533273ac434
Your Name <you@example.com>
2023-11-26 11:07:57 -0600

    Access the email address input element by ID for user input handling.

--- js/signup.js
+++ js/signup.js
@@ -3,3 +3,4 @@ document.addEventListener('DOMContentLoaded', function() {
 	const signupForm = document.getElementById('signupForm');
 	const usernameInput = document.getElementById('username');
 	const passwordInput = document.getElementById('password');
+	const emailAddressInput = document.getElementById('email_address');
