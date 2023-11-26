0ca6b2ed1a59c697c65d61fa281f0dcdc9c72227
Your Name <you@example.com>
2023-11-25 21:55:22 -0600

    Attach an event listener to the privateMessageForm for handling the submission of private messages and prevent the default form submission behavior.

--- js/private_chat.js
+++ js/private_chat.js
@@ -3,4 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	const privateMessageForm = document.getElementById('privateMessageForm');
 	const privateMessageInput = document.getElementById('privateMessageInput');
     const privateMessagesContainer = document.getElementById('privateMessages');
+	
+	 // Event listener for submitting a private message
+    privateMessageForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
 
