058c2cff15c01d1e834337ae7454aa366f8dba75
Your Name <you@example.com>
2023-11-29 19:10:33 -0600

    Attach submit event listener to profileForm for handling new profile picture submissions. Prevent the default form submission behavior.

--- js/profile_picture.js
+++ js/profile_picture.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const profileForm = document.getElementById('profileForm');
     const newProfilePictureInput = document.getElementById('new_profile_picture');
+	
+	 // Event listener for submitting a new profile picture
+     profileForm.addEventListener('submit', function(event) {
+         event.preventDefault();
+
