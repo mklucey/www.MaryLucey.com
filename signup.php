732d94980d09589add4533be2dfcae34a739eb91
Your Name <you@example.com>
2023-12-08 13:22:20 -0600

    Create SQL query for user registration in signup.php

--- signup.php
+++ signup.php
@@ -5,6 +5,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$username = $_POST['username'];
 	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 	$email = $_POST['email'];
+	
+	$sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
 
 
 ?>
