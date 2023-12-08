49efd12b90b44091ab7bfb8c5e5c7c3430306b42
Your Name <you@example.com>
2023-12-08 10:55:17 -0600

    Configure database connection parameters

--- db_connection.php
+++ db_connection.php
@@ -3,6 +3,9 @@
 $host = "sql300.ezzeblog.com"; // Replace with your MySQL Hostname
 $username = "qblog_35545457"; // Replace with your MySQL Username
 $password = "Tippy244"; // Replace with your MySQL Password
+$database = "qblog_35545457_mydatabase"; // Replace with your Database Name
+
+$conn = new mysqli($host, $username, $password, $database);
 
 
 ?>
