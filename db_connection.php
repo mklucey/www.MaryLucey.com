432aa0fe234d89f6fac476998236be763ad16a88
Your Name <you@example.com>
2023-12-08 16:14:13 -0600

    Fix MySQL connection in db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,15 +1,17 @@
 <?php
+$host = "sql300.ezzeblog.com";
+$username = "qblog_35545457";
+$password = "Tippy244";
+$database = "qblog_35545457_mydatabase";
 
-$host = "sql300.ezzeblog.com"; // Replace with your MySQL Hostname
-$username = "qblog_35545457"; // Replace with your MySQL Username
-$password = "Tippy244"; // Replace with your MySQL Password
-$database = "qblog_35545457_mydatabase"; // Replace with your Database Name
-
+// Create a new mysqli connection
 $conn = new mysqli($host, $username, $password, $database);
 
+// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
+// Optionally, set the character set to utf8 or another appropriate value
+$conn->set_charset("utf8");
 ?>
