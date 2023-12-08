6fed2aed3403a93796401dd21d0dbc8c6ff6e626
Your Name <you@example.com>
2023-12-08 16:01:48 -0600

    Fix db connection issue

--- login.php
+++ login.php
@@ -1,21 +1,24 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 session_start();
 require_once 'db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	
-	$sql = "SELECT * FROM users WHERE username = '$username'";
-	$result = $conn->query($sql);
-	
-	if ($result->num_rows > 0) {
-		$row = $result->fetch_assoc();
-		if (password_verify($password, $row['password'])) {
-			$_SESSION['user_id'] = $row['id'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['user_id'] = $row['id'];
             echo "Login successful";
-		 } else {
-             echo "Invalid password";
+        } else {
+            echo "Invalid password";
         }
     } else {
         echo "User not found";
