6cf66f7fd9e09b8a886eb7e04b0e0320e941bb19
Your Name <you@example.com>
2023-12-08 13:15:34 -0600

    Capture user email from POST data in signup.php

--- signup.php
+++ signup.php
@@ -4,6 +4,7 @@ require_once 'db_connection.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$username = $_POST['username'];
 	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	$email = $_POST['email'];
 
 
 ?>
