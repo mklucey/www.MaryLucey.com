2caf4eb59e345fdbfa24c47781ff70ffaed5b94b
Your Name <you@example.com>
2023-12-09 10:46:02 -0600

    Retrieve user information based on session user_id

--- main_chat.php
+++ main_chat.php
@@ -7,5 +7,9 @@ if (!isset($_SESSION['user_id'])) {
 
 require_once 'db_connection.php';
 
+$user_id = $_SESSION['user_id'];
+$sql = "SELECT * FROM users WHERE user_id = '$user_id'";
+$result = $conn->query($sql);
+
 
 ?>
