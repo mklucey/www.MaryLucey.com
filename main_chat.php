4bf3d85f722425a7d1c7414446b0486c89def457
Your Name <you@example.com>
2023-12-09 10:50:05 -0600

    Check and fetch user data from database result

--- main_chat.php
+++ main_chat.php
@@ -11,5 +11,8 @@ $user_id = $_SESSION['user_id'];
 $sql = "SELECT * FROM users WHERE user_id = '$user_id'";
 $result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+
 
 ?>
