cc4eebf7047cb37953f93fc57a21ee72366cdabd
Your Name <you@example.com>
2023-12-09 10:58:14 -0600

    Display online users in the main chat interface

--- main_chat.php
+++ main_chat.php
@@ -17,6 +17,18 @@ if ($result->num_rows > 0) {
 	// Display online users
     $sqlAllUsers = "SELECT * FROM users WHERE user_id != '$user_id'";
     $resultAllUsers = $conn->query($sqlAllUsers);
+	
+	echo "<h1>Main Chat</h1>";
+
+    if ($resultAllUsers->num_rows > 0) {
+        echo "Users online:<br>";
+        while ($row = $resultAllUsers->fetch_assoc()) {
+            echo $row['username'] . "<br>";
+        }
+    } else {
+        echo "No users online.";
+    }
+
 
 
 
