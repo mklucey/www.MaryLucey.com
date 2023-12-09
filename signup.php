7bb909f68345d3ac2f6ada42dfe1fa39cbf72ee6
Your Name <you@example.com>
2023-12-09 17:22:27 -0600

    Capture and secure user input: Extract username, hash password using bcrypt, and obtain email from POST data.

--- signup.php
+++ signup.php
@@ -2,6 +2,9 @@
 require_once 'db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $email = $_POST['email'];
 
 
 ?>
