5e3b2bbbc850a9c13d9539e1157638abfadfe990
Your Name <you@example.com>
2023-12-09 14:25:56 -0600

    Verify the connection and terminate with an error message if unsuccessful.

--- db_connection.php
+++ db_connection.php
@@ -8,6 +8,10 @@ $database = "chat_app";
 // Create a new mysqli connection
 $conn = new mysqli($host, $username, $password, $database);
 
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 
 ?>
