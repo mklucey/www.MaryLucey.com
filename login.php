c0025b3242980fd32a6551809391b4f4ada26d16
Your Name <you@example.com>
2023-12-09 20:29:31 -0600

    Execute SQL query to select user data from 'users' table based on the provided username.

--- login.php
+++ login.php
@@ -5,5 +5,9 @@ require_once 'db_connection.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
+	
+	$sql = "SELECT * FROM users WHERE username = '$username'";
+    $result = $conn->query($sql);
+
 
 ?>
