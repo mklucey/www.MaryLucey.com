d048a499340d3f4c7d520e61068287b5dd1a1f2a
Your Name <you@example.com>
2023-12-10 14:11:05 -0600

    If user information is found, retrieve and display private messages for the current user based on the receiver ID.

--- private_chat.php
+++ private_chat.php
@@ -11,6 +11,22 @@ $user_id = $_SESSION['user_id'];
 $sql = "SELECT * FROM users WHERE id = '$user_id'";
 $result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+    // Implement private chat page functionality here
+
+    // Sample: Display private messages
+    $sqlPrivateMessages = "SELECT * FROM messages WHERE receiver_id = '$user_id'";
+    $resultPrivateMessages = $conn->query($sqlPrivateMessages);
+
+    if ($resultPrivateMessages->num_rows > 0) {
+        echo "Private messages:<br>";
+        while ($row = $resultPrivateMessages->fetch_assoc()) {
+            echo $row['message'] . "<br>";
+        }
+    } else {
+        echo "No private messages.";
+    }
 
 
 
