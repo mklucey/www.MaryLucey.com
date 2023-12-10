1f6961b1e8a24cba1b1e16c95d465e9c13f0191e
Your Name <you@example.com>
2023-12-09 21:01:52 -0600

    Fetch user details from the 'users' table based on the session user ID. Display the main chat header and retrieve a list of online users excluding the current user.

--- main_chat.php
+++ main_chat.php
@@ -11,6 +11,15 @@ $user_id = $_SESSION['user_id'];
 $sql = "SELECT * FROM users WHERE id = '$user_id'";
 $result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+
+    // Display online users
+    $sqlAllUsers = "SELECT * FROM users WHERE id != '$user_id'";
+    $resultAllUsers = $conn->query($sqlAllUsers);
+
+    echo "<h1>Main Chat</h1>";
+
 
 
 ?>
