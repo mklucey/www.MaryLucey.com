e33950258144dc1d0435c70a23d718d537039cf6
Your Name <you@example.com>
2023-12-09 20:33:52 -0600

    Check if user exists based on the provided username. If found, verify the password using password_verify. Set user session ID upon successful login; otherwise, provide appropriate error messages.

--- login.php
+++ login.php
@@ -8,6 +8,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	
 	$sql = "SELECT * FROM users WHERE username = '$username'";
     $result = $conn->query($sql);
+	
+	 if ($result->num_rows > 0) {
+         $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['user_id'] = $row['id'];
+            echo "Login successful";
+        } else {
+            echo "Invalid password";
+        }
+    } else {
+        echo "User not found";
+    }
+}
 
 
 ?>
