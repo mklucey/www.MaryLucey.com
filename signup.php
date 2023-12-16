768b903d7fa1e149eb2ba831a07b25f7c75e4fdd
Your Name <you@example.com>
2023-12-16 14:47:59 -0600

    Fixed header modification error in signup.php and added redirect after successful signup.

--- signup.php
+++ signup.php
@@ -2,41 +2,30 @@
 include 'db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$email = $_POST['email'];
-	
-	// Validate input
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    // Validate input
     if (empty($username) || empty($password) || empty($email)) {
         echo "Signup failed. Please fill out all fields.";
-	} else {
+    } else {
         // Hash the password (use a secure hashing algorithm)
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-		
-		 // Insert user data into the database
+
+        // Insert user data into the database
         $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$hashedPassword', '$email')";
         $result = mysqli_query($conn, $sql);
-		
-		 if ($result) {
-             echo "Signup successful. You can now login.";
-             header('Location: login.html'); // Redirect to login page
-             exit();
-		 } else {
-            echo "Signup failed. Please try again.";
+
+        if ($result) {
+            header('Location: login.html'); // Redirect to login page
+            echo "Signup successful. You can now login.";
+            exit();
+        } else {
             header('Location: signup.html'); // Redirect to signup page
+            echo "Signup failed. Please try again.";
             exit();
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
 ?>
