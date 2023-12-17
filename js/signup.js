af536bbd225feceebb006c09d3e2ab2705593ad1
Your Name <you@example.com>
2023-12-16 23:07:41 -0600

    Enhance signup.js for improved response handling and redirection

--- js/signup.js
+++ js/signup.js
@@ -1,32 +1,34 @@
 $(document).ready(function () {
-	$('#signupForm').submit(function (event) {
-		event.preventDefault();
-		
-		var username = $('#username').val();
-		var password = $('#password').val();
-		var email = $('#email').val();
-		
-	    $.ajax({
-			type: 'POST',
-			url: 'signup.php',
-			 data: {
+    $('#signupForm').submit(function (event) {
+        event.preventDefault();
+
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var email = $('#email').val();
+
+        $.ajax({
+            type: 'POST',
+            url: 'signup.php',
+            data: {
                 username: username,
                 password: password,
                 email: email
             },
-			 success: function (response) {
-                if (response === "success") {
-					alert('Sign up successfully completed');
+            success: function (response) {
+                var parts = response.split("|");
+                var status = parts[0];
+                var message = parts[1];
+
+                if (status === "success") {
+                    alert(message);
                     window.location.href = 'login.html'; // Redirect to login page
-				} else {
-                    $('#signupError').text(response);
+                } else {
+                    $('#signupError').text(message);
                 }
             },
-			 error: function () {
+            error: function () {
                 alert('An error occurred during the signup process. Please try again.');
             }
         });
     });
 });
-			
-			
