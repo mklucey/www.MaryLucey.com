b864b1af309d359af217adf67fcdc479d39e042d
Your Name <you@example.com>
2023-12-16 18:24:33 -0600

    Handle success in the Ajax request and check if the response is equal to 'success'.

--- js/signup.js
+++ js/signup.js
@@ -14,5 +14,7 @@ $(document).ready(function () {
                 password: password,
                 email: email
             },
+			 success: function (response) {
+                if (response === "success") {
 			
 			
