5d77bc4706c24d72ecd8dc4340ee8c34e8b043b1
Your Name <you@example.com>
2023-12-18 17:17:04 -0600

    feat: Implement dynamic chat functionality

--- js/main_chat.js
+++ js/main_chat.js
@@ -1,35 +1,65 @@
-// Handle chat form submission
-$('#chatForm').submit(function (event) {
-    event.preventDefault();
-
-    var message = $('#message').val();
-    var receiverId = $('#userList div.active').data('user-id');
-
-    // Perform AJAX request to handle the form submission in the same main_chat.php file
+$(document).ready(function () {
+    // Fetch initial chat data
     $.ajax({
         url: 'main_chat.php',
-        method: 'POST',
-        data: { message: message, receiver_id: receiverId },
+        method: 'GET',
         dataType: 'json',
-        success: function (response) {
-            // Check if the response is valid JSON
-            if (response && typeof response === 'object') {
-                // Handle success response
-                console.log(response);
+        success: function (data) {
+            // Display current user's username
+            $('#currentUsername').text(data.currentUserData.username);
 
-                // Assuming the response includes the newly sent message data
-                var newMessageHtml = '<div><strong>' + response.sender_username + ':</strong> ' + response.content + ' (' + response.timestamp + ')</div>';
-                $('#chatMessages').prepend(newMessageHtml); // Prepend the new message to the chat area
-            } else {
-                console.error('Invalid JSON response:', response);
-            }
+            // Display user list
+            var userListHtml = '';
+            data.userList.forEach(function (user) {
+                userListHtml += '<div data-user-id="' + user.id + '">' + user.username + '</div>';
+            });
+            $('#userList').html(userListHtml);
+
+            // Display chat messages
+            var chatMessagesHtml = '';
+            data.chatMessages.forEach(function (message) {
+                chatMessagesHtml += '<div><strong>' + message.sender_username + ':</strong> ' + message.content + ' (' + message.timestamp + ')</div>';
+            });
+            $('#chatMessages').html(chatMessagesHtml);
         },
         error: function (jqXHR, textStatus, errorThrown) {
-            // Log specific error details to the console
-            console.error('Error submitting chat message:', textStatus, errorThrown);
-
-            // Log the response text for further debugging
+            console.error('Error fetching initial chat data:', textStatus, errorThrown);
             console.log(jqXHR.responseText);
         }
+    });
+
+    // Handle chat form submission
+    $('#chatForm').submit(function (event) {
+        event.preventDefault();
+
+        var message = $('#message').val();
+        var receiverId = $('#userList div.active').data('user-id');
+
+        // Perform AJAX request to handle the form submission in the same main_chat.php file
+        $.ajax({
+            url: 'main_chat.php',
+            method: 'POST',
+            data: { message: message, receiver_id: receiverId },
+            dataType: 'json',
+            success: function (response) {
+                try {
+                    // Check if the response is valid JSON
+                    if (response && typeof response === 'object') {
+                        // Assuming the response includes the newly sent message data
+                        var newMessageHtml = '<div><strong>' + response.sender_username + ':</strong> ' + response.content + ' (' + response.timestamp + ')</div>';
+                        $('#chatMessages').prepend(newMessageHtml); // Prepend the new message to the chat area
+                    } else {
+                        console.error('Invalid JSON response:', response);
+                    }
+                } catch (error) {
+                    console.error('Error handling response:', error);
+                    console.log('Raw response:', response);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error('Error submitting chat message:', textStatus, errorThrown);
+                console.log(jqXHR.responseText);
+            }
+        });
     });
 });
