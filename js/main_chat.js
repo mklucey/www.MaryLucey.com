2c904c86c13738f1bdb450bd7cac4fcade2bbd8f
Your Name <you@example.com>
2023-12-17 18:02:24 -0600

    Retrieve and display current user's data on successful AJAX request

--- js/main_chat.js
+++ js/main_chat.js
@@ -1,2 +1,8 @@
 $(document).ready(function() {
 	// Fetch and display current user's data
+	 $.ajax({
+        url: 'main_chat.php',
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            $('#currentUsername').text(data.currentUserData.username);
