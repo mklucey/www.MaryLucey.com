6d3f03338d1498cc4cda8e40b65df23d6a0e7dc4
Your Name <you@example.com>
2023-12-20 15:57:27 -0600

    fix: Correct database connection credentials in db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,15 +4,10 @@ $username = "4417646_chatapp";
 $password = ":MUF-z-m7)%yqN:T";
 $database = "4417646_chatapp";
 
-$conn = mysqli_connect($servername, $username, $password, $database);
+// Use htmlspecialchars to prevent sensitive data exposure
+$conn = mysqli_connect($servername, $username, htmlspecialchars($password), $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
-
-
-
-
 ?>
