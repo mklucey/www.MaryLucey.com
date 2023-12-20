e1f0cb70849a17a85e5ed70e1e547c764a85f741
Your Name <you@example.com>
2023-12-20 17:57:29 -0600

    Remove htmlspecialchars from password assignment in database connection

--- db_connection.php
+++ db_connection.php
@@ -4,8 +4,8 @@ $username = "4417646_chatapp";
 $password = "tdzOGRqM9Cl6C?@p";
 $database = "4417646_chatapp";
 
-// Use htmlspecialchars to prevent sensitive data exposure
-$conn = mysqli_connect($servername, $username, htmlspecialchars($password), $database);
+$conn = mysqli_connect($servername, $username, $password, $database);
+
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
