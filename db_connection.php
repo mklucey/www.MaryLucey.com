77ce33a98cc27ef54645825f23c0f44e3a41186d
Your Name <you@example.com>
2023-12-20 15:47:16 -0600

    Update db credentials

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "fdb1032.awardspace.net";
 $username = "4417646_chatapp";
-$password = "Rippy324";
+$password = ":MUF-z-m7)%yqN:T";
 $database = "4417646_chatapp";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
