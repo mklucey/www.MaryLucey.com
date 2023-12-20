5535d4dd337e004a061c3e4563ca877df3ba52a0
Your Name <you@example.com>
2023-12-20 15:14:38 -0600

    Handle connection failure and terminate with error message

--- db_connection.php
+++ db_connection.php
@@ -6,6 +6,9 @@ $database = "4417646_chatapp";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 
 
