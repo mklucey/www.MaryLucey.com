c45bbe5f1494178526621a1820a7c0b40351cb21
Your Name <you@example.com>
2023-12-26 15:35:56 -0600

    Validate input; if 'username', 'password,' or 'email' is empty, display an alert with the message 'Signup failed. Please fill out all fields.

--- js/signup.js
+++ js/signup.js
@@ -5,3 +5,7 @@ $(document).ready(function () {
 		var username = $('#username').val();
         var password = $('#password').val();
         var email = $('#email').val();
+		
+		 // Validate input
+        if (username === '' || password === '' || email === '') {
+            alert("Signup failed. Please fill out all fields.");
