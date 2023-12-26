eaed9a02af957d2fddcff33057e39738f4e55000
Your Name <you@example.com>
2023-12-26 15:43:36 -0600

    If the input is valid, send data to the server using AJAX with a POST request to 'signup.php,' including 'username,' 'password (plain password), and 'email' inthe data.

--- js/signup.js
+++ js/signup.js
@@ -9,3 +9,13 @@ $(document).ready(function () {
 		 // Validate input
         if (username === '' || password === '' || email === '') {
             alert("Signup failed. Please fill out all fields.");
+		 } else {
+            // Send data to the server using AJAX
+            $.ajax({
+                type: 'POST',
+                url: 'signup.php',
+                data: {
+                    username: username,
+                    password: password, // Send the plain password
+                    email: email
+                },
