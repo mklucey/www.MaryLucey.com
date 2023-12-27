caa6be6e08cfd33674984887c2892ee5b80fb854
Your Name <you@example.com>
2023-12-26 19:47:22 -0600

    If input is valid, use a prepared statement to prevent SQL injection. Select data from the 'login' table where the 'username' matches the input. Execute the statement and get the result.

--- login.php
+++ login.php
@@ -8,6 +8,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	// Validate input
     if (empty($username) || empty($password)) {
         echo "Login failed. Please enter both username and password.";
+	} else {
+        // Use prepared statement to prevent SQL injection
+        $stmt = $conn->prepare("SELECT * FROM login WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
 
 
 ?>
