d61f7c9124f7b689c86ba326077669bff9195a28
Your Name <you@example.com>
2023-12-26 21:34:28 -0600

    fix: Update AJAX request to use form serialization in login.js

--- js/login.js
+++ js/login.js
@@ -2,14 +2,12 @@ $(document).ready(function () {
     $('#loginForm').submit(function (event) {
         event.preventDefault();
 
-        var formData = new FormData(this); // Create a FormData object with the form data
+        var formData = $(this).serialize(); // Serialize the form data
 
         $.ajax({
             type: 'POST',
             url: 'login.php',
             data: formData,
-            processData: false,  // Prevent jQuery from automatically processing the data
-            contentType: false,  // Prevent jQuery from automatically setting the content type
             success: function (response) {
                 alert(response);
 
