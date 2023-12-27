741ccbb39f5b98d3dc45aca92d576f75927de17d
Your Name <you@example.com>
2023-12-26 19:40:24 -0600

    Validate input; if 'username' or 'password' is empty, display 'Login failed. Please enter both username and password.'

--- login.php
+++ login.php
@@ -4,6 +4,10 @@ include 'db_connection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
+	
+	// Validate input
+    if (empty($username) || empty($password)) {
+        echo "Login failed. Please enter both username and password.";
 
 
 ?>
