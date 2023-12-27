52f9f10b4b04f85e6baa768f5701893e0b4f90ed
Your Name <you@example.com>
2023-12-26 20:15:55 -0600

    Retrieve values of 'username' and 'password' from their respective input fields.

--- js/login.js
+++ js/login.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
     $('#loginForm').submit(function (event) {
         event.preventDefault();
+		
+		var username = $('#username').val();
+        var password = $('#password').val();
+
