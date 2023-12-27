626078e5b8d743f83fd0cd24f81eb5a56c899f38
Your Name <you@example.com>
2023-12-27 09:33:19 -0600

    Added 'use strict'; at the beginning ofthe script.

--- js/login.js
+++ js/login.js
@@ -1,9 +1,16 @@
+// Use strict mode to catch common coding mistakes
+'use strict';
+
 $(document).ready(function () {
+    // Form submission event
     $('#loginForm').submit(function (event) {
+        // Prevent the default form submission
         event.preventDefault();
 
+        // Create a FormData object with the form data
         var formData = new FormData(this);
 
+        // AJAX request
         $.ajax({
             type: 'POST',
             url: 'login.php',
@@ -11,14 +18,18 @@ $(document).ready(function () {
             contentType: false,
             processData: false,
             success: function (response) {
+                // Display response
                 alert(response);
 
+                // Check if the response contains success message
                 if (response.includes('Login successful. Enjoy chatting.')) {
+                    // Redirect to the main chat page
                     window.location.href = 'main_chat.html';
                 }
             },
             error: function () {
-                alert('Usename and/or password is incorrect. Please try again.');
+                // Display error message
+                alert('Username and/or password is incorrect. Please try again.');
             }
         });
     });
