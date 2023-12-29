3376d51aaa29b848a36207e7e64221988cd2ea27
Your Name <you@example.com>
2023-12-28 21:36:16 -0600

    Update login.js for consistent JSON response handling.

--- js/login.js
+++ js/login.js
@@ -1,31 +1,28 @@
-'use strict';
-
 $(document).ready(function () {
     $('#loginForm').submit(function (event) {
         event.preventDefault();
 
-        var formData = new FormData(this);
+        var username = $('#username').val();
+        var password = $('#password').val();
 
         $.ajax({
             type: 'POST',
             url: 'login.php',
-            data: formData,
-            contentType: false,
-            processData: false,
-            success: function (response) {
-                console.log('AJAX success:', response);
-
-                // Display success message
-                alert('Login successful. Enjoy chatting.');
-
-                // Redirect to main_chat.html
-                window.location.href = 'main_chat.html';
+            data: {
+                username: username,
+                password: password
             },
-            error: function (error) {
-                console.error('AJAX error:', error);
+            dataType: 'json', // Specify the expected data type
+            success: function (response) {
+                alert(response.message); // Display the response message
 
-                // Display error message
-                alert('Login failed. Please try again.');
+                if (response.status === 'success') {
+                    // Redirect to the main chat page upon successful login
+                    window.location.href = 'main_chat.html';
+                }
+            },
+            error: function () {
+                alert('Error submitting the form');
             }
         });
     });
