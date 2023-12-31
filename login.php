b8d81357a400b2294196724ed4d073d929e058d6
Your Name <you@example.com>
2023-12-31 09:41:18 -0600

    Create login.php: Implement server-side login logic with database connection.

--- login.php
+++ login.php
@@ -1,42 +1,21 @@
 <?php
-session_start();
+// login.php
+
 include 'db_connection.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-    // Validate input
-    if (empty($username) || empty($password)) {
-        $response = array("status" => "error", "message" => "Login failed. Please enter both username and password.");
+    $sql = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "Login successful. Enjoy chatting";
     } else {
-        // Use prepared statement to prevent SQL injection
-        $stmt = $conn->prepare("SELECT * FROM login WHERE username = ?");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result && $result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-
-            // Verify the password
-            if (password_verify($password, $row['password'])) {
-                // Store user data in the session
-                $_SESSION['user_id'] = $row['id'];
-                $_SESSION['username'] = $row['username'];
-
-                // Display success message
-                $response = array("status" => "success", "message" => "Login successful. Enjoy chatting.");
-            } else {
-                $response = array("status" => "error", "message" => "Username and/or password is incorrect. Please try again.");
-            }
-        } else {
-            $response = array("status" => "error", "message" => "Username and/or password is incorrect. Please try again.");
-        }
-        $stmt->close();
+        echo "Login failed. Please try again";
     }
-
-    // Send JSON response
-    echo json_encode($response);
 }
+
+$conn->close();
 ?>
