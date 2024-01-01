33b0a424a415c27372a7885698a2e1b237ef2137
Your Name <you@example.com>
2023-12-31 22:30:20 -0600

    Debug login.php for direct testing

--- login.php
+++ login.php
@@ -7,6 +7,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
+    echo "Received username: $username, password: $password"; // Debugging output
+    
     $sql = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
     $result = $conn->query($sql);
 
